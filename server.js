2ff3f2bca8242c996539afe0f6533e1b91fefe73
Llorch <skylakebeta@gmail.com>
2016-12-15 23:45:28 -0800

    Inital commit/push

---
+++ server.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+var https = require('https');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var cors = require('cors');
+
+var options = {
+   key  : fs.readFileSync('./certificates/server.key'),
+   cert : fs.readFileSync('./certificates/server.crt')
+};
+
+// Serve assets in /public.
+app.use(express.static(__dirname + '/public'));
+
+// So we can POST.
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// Since Mixmax calls this API directly from the client-side, it must be whitelisted.
+var corsOptions = {
+  origin: /^[^.\s]+\.mixmax\.com$/,
+  credentials: true
+};
+
+// The editor interface.
+app.get('/editor', function(req, res) {
+  res.sendFile(__dirname + '/editor.html');
+});
+
+// The in-email representation.
+app.post('/api/resolver', cors(corsOptions), require('./api/resolver'));
+
+// app.listen(process.env.PORT || 8910);
+
+https.createServer(options, app).listen(8910, function () {
+   console.log('Started!');
+});
